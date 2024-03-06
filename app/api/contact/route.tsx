4efb57b5a1627de13bc6aaecd4a22ccbb5d52a34
Your Name <you@example.com>
2024-03-06 19:29:28 +0530

    modified:   app/api/contact/route.tsx 	new file:   mailBody.ts

--- app/api/contact/route.tsx
+++ app/api/contact/route.tsx
@@ -1,16 +1,14 @@
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
+import { mailBody } from "@/mailBody";
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://shahbajalam-portfolio.netlify.app",
-];
+const allowedOrigins = [process.env.DEV, process.env.PROD];
 
 const POST = async (req: Request) => {
     const origin = req.headers.get("origin");
     const { fname, email, message } = await req.json();
 
-    if (origin && !allowedOrigins.includes(origin)) {
+    if (!origin || (origin && !allowedOrigins.includes(origin))) {
         return new NextResponse(null, {
             status: 400,
             statusText: "Bad Request",
@@ -18,39 +16,12 @@ const POST = async (req: Request) => {
     }
 
     try {
-        const resend = new Resend("re_BY6yRV8T_AnYNXw1HVrmuhyBXRj7yb6Co");
+        const resend = new Resend(process.env.API_KEY);
         const { data, error } = await resend.emails.send({
-            from: "onboarding@resend.dev",
-            to: "shahbajalam78@gmail.com",
+            from: process.env.FROM,
+            to: process.env.TO,
             subject: `New message from ${fname}`,
-            html: `
-                <div
-                    style="
-                        font-family: sans-serif;
-                        border-radius: 15px;
-                        background: linear-gradient(to top, #181717, #313130);
-                        color: aliceblue;
-                        overflow: hidden;
-                    "
-                >
-                    <div style="padding: 1rem">
-                        <h3 style="margin: 0">Hi Shahbaj Alam,</h3>
-                    </div>
-                    <div>
-                        <h3 style="padding: 2rem 1rem; margin: 0">${message}</h3>
-                        <p
-                            style="
-                                text-decoration: none;
-                                margin: 0;
-                                padding: 1rem;
-                                font-size: 1.1rem;
-                            "
-                        >
-                        ${email}
-                        </p>
-                    </div>
-                </div>
-            `,
+            html: mailBody(message, email),
         });
 
         if (error)
@@ -62,6 +33,7 @@ const POST = async (req: Request) => {
             headers: {
                 "Access-Control-Allow-Origin": origin,
                 "Access-Control-Allow-Methods": "POST",
+                "Content-Type": "application/json",
             },
         });
     } catch (error) {
